261fe3ed4b437439b009a32392f978104e697f5c
sumikarim <sumairapkarim@gmail.com>
2024-07-03 16:27:18 +0100

    Updated JS for search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelectorAll("#search-input");
-  let cityElement = document.querySelectorAll("#weather-city");
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = searchInput.value;
 }
 
-let searchFormElement = document.querySelectorAll("#search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
